fe490174da8c886144234b936a8a96f3db2d4f3d
GMaiolo <guille.maiolo@gmail.com>
2020-02-06 19:09:47 -0300

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <div class="columns">
+      <div class="column">
+        <div class="notification is-primary has-text-centered">
+          <p class="title">Columna 1</p>
+        </div>
+      </div>
+      <div class="column">
+        <div class="notification is-primary has-text-centered">
+          <p class="title">Columna 2</p>
+        </div>
+      </div>
+      <div class="column">
+        <div class="notification is-primary has-text-centered">
+          <p class="title">Columna 3</p>
+        </div>
+      </div>
+      <div class="column">
+        <div class="notification is-primary has-text-centered">
+          <p class="title">Columna 4</p>
+        </div>
+      </div>
+      <div class="column">
+        <div class="notification is-primary has-text-centered">
+          <p class="title">Columna 5</p>
+        </div>
+      </div>
+      <div class="column">
+        <div class="notification is-primary has-text-centered">
+          <p class="title">Columna 6</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home"
+};
+</script>
