0b76da649a49387c329efde3abc2584b484cea9e
julietaflux <julietaflux@gmail.com>
2020-03-22 12:08:41 -0300

    Feat: register form options

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,56 +1,68 @@
 <template>
   <div>
     <div class="field">
-      <label class="label">Name</label>
+      <label class="label">Tipo</label>
       <div class="control">
-        <input class="input" type="text" placeholder="Text input" />
-      </div>
-    </div>
-
-    <div class="field">
-      <label class="label">Username</label>
-      <div class="control has-icons-left has-icons-right">
-        <input class="input is-success" type="text" placeholder="Text input" value="bulma" />
-        <span class="icon is-small is-left">
-          <i class="fas fa-user"></i>
-        </span>
-        <span class="icon is-small is-right">
-          <i class="fas fa-check"></i>
-        </span>
-      </div>
-      <p class="help is-success">This username is available</p>
-    </div>
-
-    <div class="field">
-      <label class="label">Email</label>
-      <div class="control has-icons-left has-icons-right">
-        <input class="input is-danger" type="email" placeholder="Email input" value="hello@" />
-        <span class="icon is-small is-left">
-          <i class="fas fa-envelope"></i>
-        </span>
-        <span class="icon is-small is-right">
-          <i class="fas fa-exclamation-triangle"></i>
-        </span>
-      </div>
-      <p class="help is-danger">This email is invalid</p>
-    </div>
-
-    <div class="field">
-      <label class="label">Subject</label>
-      <div class="control">
-        <div class="select">
-          <select>
-            <option>Select dropdown</option>
-            <option>With options</option>
-          </select>
+        <div class="field">
+          <div class="field">
+            <input
+              class="is-checkradio has-background-color is-primary"
+              id="exampleCheckboxBackgroundColorSuccess1"
+              type="checkbox"
+              name="exampleCheckboxBackgroundColorSuccess1"
+              checked="checked"
+            />
+            <label for="exampleCheckboxBackgroundColorSuccess1">Puedo adoptar</label>
+          </div>
+          <div class="field">
+            <input
+              class="is-checkradio has-background-color is-primary"
+              id="exampleCheckboxBackgroundColorSuccess2"
+              type="checkbox"
+              name="exampleCheckboxBackgroundColorSuccess2"
+              checked="checked"
+            />
+            <label for="exampleCheckboxBackgroundColorSuccess2">Puedo dar tránsito</label>
+          </div>
+          <div class="field">
+            <input
+              class="is-checkradio has-background-color is-primary"
+              id="exampleCheckboxBackgroundColorSuccess3"
+              type="checkbox"
+              name="exampleCheckboxBackgroundColorSuccess3"
+              checked="checked"
+            />
+            <label for="exampleCheckboxBackgroundColorSuccess3">Puedo hacer viajes</label>
+          </div>
         </div>
       </div>
     </div>
-
+    <div class="is-divider"></div>
     <div class="field">
-      <label class="label">Message</label>
+      <label class="label">Voluntariado</label>
       <div class="control">
-        <textarea class="textarea" placeholder="Textarea"></textarea>
+        <input
+          id="switchExample"
+          type="checkbox"
+          name="switchExample"
+          class="switch"
+          checked="checked"
+        />
+        <label for="switchExample">Quiero ser voluntario en refugios cercanos</label>
+      </div>
+    </div>
+    <div class="is-divider"></div>
+    <div class="field">
+      <label class="label">Alertas</label>
+      <div class="control">
+        <input
+          id="switchExample"
+          type="checkbox"
+          name="switchExample"
+          class="switch"
+          checked="checked"
+        />
+        <label for="switchExample">Quiero recibir alertas sobre casos de riesgo cercanos</label>
       </div>
     </div>
   </div>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="container">
     <div class="steps" id="stepsDemo">
-      <div class="step-item is-active is-success">
+      <div class="step-item is-active is-primary">
         <div class="step-marker">1</div>
         <div class="step-details">
-          <p class="step-title">Account</p>
+          <p class="step-title">Cuenta</p>
         </div>
       </div>
       <div class="step-item">
         <div class="step-marker">2</div>
         <div class="step-details">
-          <p class="step-title">Profile</p>
+          <p class="step-title">Perfil</p>
         </div>
       </div>
       <div class="step-item">
@@ -22,7 +22,7 @@
       <div class="step-item">
         <div class="step-marker">4</div>
         <div class="step-details">
-          <p class="step-title">Finish</p>
+          <p class="step-title">Terminar</p>
         </div>
       </div>
       <div class="steps-content">
@@ -35,118 +35,13 @@
             </div>
           </div>
         </div>
-        <div class="step-content has-text-centered">
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">Firstname</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <div class="control">
-                  <input
-                    class="input"
-                    name="firstname"
-                    id="firstname"
-                    type="text"
-                    placeholder="Firstname"
-                    autofocus
-                    data-validate="require"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">Last name</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <div class="control has-icon has-icon-right">
-                  <input
-                    class="input"
-                    type="text"
-                    name="lastname"
-                    id="lastname"
-                    placeholder="Last name"
-                    data-validate="require"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">Email</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <div class="control has-icon has-icon-right">
-                  <input
-                    class="input"
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="Email"
-                    data-validate="require"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="step-content has-text-centered">
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">Facebook account</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <div class="control">
-                  <input
-                    class="input"
-                    name="facebook"
-                    id="facebook"
-                    type="text"
-                    placeholder="Facebook account url"
-                    autofocus
-                    data-validate="require"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="field is-horizontal">
-            <div class="field-label is-normal">
-              <label class="label">Twitter account</label>
-            </div>
-            <div class="field-body">
-              <div class="field">
-                <div class="control">
-                  <input
-                    class="input"
-                    name="twitter"
-                    id="twitter"
-                    type="text"
-                    placeholder="Twitter account url"
-                    autofocus
-                    data-validate="require"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="step-content has-text-centered">
-          <h1 class="title is-4">Your account is now created!</h1>
-        </div>
       </div>
       <div class="steps-actions">
         <div class="steps-action">
-          <a href="#" data-nav="previous" class="button is-light">Previous</a>
+          <a href="#" data-nav="previous" class="button is-light">Anterior</a>
         </div>
         <div class="steps-action">
-          <a href="#" data-nav="next" class="button is-light">Next</a>
+          <a href="#" data-nav="next" class="button is-light">Siguiente</a>
         </div>
       </div>
     </div>
