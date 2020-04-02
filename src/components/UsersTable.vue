f0a4cec18482830ed26d897a549212aefb65037e
GMaiolo <guille.maiolo@gmail.com>
2020-04-02 18:58:15 -0300

    Fix: UsersTable Cards rewrite

--- src/components/UsersTable.vue
+++ src/components/UsersTable.vue
@@ -1,82 +1,112 @@
 <template>
-  <div class="pricing-table">
-    <div class="pricing-plan is-info">
-      <div class="plan-header">Refugio</div>
-      <div class="plan-price">
-        <figure class="image is-48x48 inline-block m-auto">
-          <img src="../assets/png/035-cat-1.png" />
-        </figure>
-      </div>
-      <div class="plan-items">
-        <div class="plan-item">
-          <strong>¡ES GRATIS!</strong>
+  <div>
+    <div class="lg:flex">
+      <!-- Refugio -->
+      <div class="flex-1 m-2 card text-center">
+        <header class="p-4">
+          <span class="title is-5">Refugio</span>
+          <figure class="image is-48x48 mt-4 mx-auto">
+            <img src="../assets/png/035-cat-1.png" />
+          </figure>
+        </header>
+        <div class="content">
+          <div class="bg-gray-100 border-b-2 border-white p-2">
+            <strong>¡ES GRATIS!</strong>
+          </div>
+          <div class="bg-gray-100 border-b-2 border-white p-2">
+            <span class="subtitle is-6">Publicar mascotas</span>
+          </div>
+          <div class="bg-gray-100 border-b-2 border-white p-2">
+            Contactar adoptantes
+          </div>
+          <div class="bg-gray-100 border-b-2 border-white p-2">
+            Recibir donaciones
+          </div>
+          <footer class="p-2">
+            <button class="button is-fullwidth is-info">Ver más</button>
+          </footer>
         </div>
-        <div class="plan-item">Publicar mascotas</div>
-        <div class="plan-item">Contactar adoptantes</div>
-        <div class="plan-item">Recibir donaciones</div>
       </div>
-      <div class="plan-footer">
-        <button class="button is-fullwidth">Ver más</button>
-      </div>
-    </div>
 
-    <div class="pricing-plan is-active">
-      <div class="plan-header">Adoptante</div>
-      <div class="plan-price">
-        <figure class="image is-48x48 inline-block m-auto">
-          <img src="../assets/png/001-smartphone.png" />
-        </figure>
-      </div>
-      <div class="plan-items">
-        <div class="plan-item">
-          <strong>¡ES GRATIS!</strong>
+      <!-- Adoptante -->
+      <div class="flex-1 m-2 card text-center">
+        <header class="p-4">
+          <span class="title is-5">Refugio</span>
+          <figure class="image is-48x48 mt-4 mx-auto">
+            <img src="../assets/png/001-smartphone.png" />
+          </figure>
+        </header>
+        <div class="content">
+          <div class="bg-gray-100 border-b-2 border-white p-2">
+            <strong>¡ES GRATIS!</strong>
+          </div>
+          <div class="bg-gray-100 border-b-2 border-white p-2">
+            <span class="subtitle is-6">Buscar mascotas</span>
+          </div>
+          <div class="bg-gray-100 border-b-2 border-white p-2">
+            Adoptar
+          </div>
+          <div class="bg-gray-100 border-b-2 border-white p-2">
+            Dar tránsito
+          </div>
+          <footer class="p-2">
+            <button class="button is-fullwidth is-primary">Ver más</button>
+          </footer>
         </div>
-        <div class="plan-item">Buscar mascotas</div>
-        <div class="plan-item">Adoptar</div>
-        <div class="plan-item">Dar tránsito</div>
       </div>
-      <div class="plan-footer">
-        <button class="button is-fullwidth">Ver más</button>
-      </div>
-    </div>
 
-    <div class="pricing-plan is-warning">
-      <div class="plan-header">Voluntario</div>
-      <div class="plan-price">
-        <figure class="image is-48x48 inline-block m-auto">
-          <img src="../assets/png/volunteer.png" />
-        </figure>
-      </div>
-      <div class="plan-items">
-        <div class="plan-item">
-          <strong>¡ES GRATIS!</strong>
+      <!-- Voluntario -->
+      <div class="flex-1 m-2 card text-center">
+        <header class="p-4">
+          <span class="title is-5">Refugio</span>
+          <figure class="image is-48x48 mt-4 mx-auto">
+            <img src="../assets/png/volunteer.png" />
+          </figure>
+        </header>
+        <div class="content">
+          <div class="bg-gray-100 border-b-2 border-white p-2">
+            <strong>¡ES GRATIS!</strong>
+          </div>
+          <div class="bg-gray-100 border-b-2 border-white p-2">
+            <span class="subtitle is-6">Ofrecer traslados</span>
+          </div>
+          <div class="bg-gray-100 border-b-2 border-white p-2">
+            Realizar donaciones
+          </div>
+          <div class="bg-gray-100 border-b-2 border-white p-2">
+            Ayudar en emergencias
+          </div>
+          <footer class="p-2">
+            <button class="button is-fullwidth is-warning">Ver más</button>
+          </footer>
         </div>
-        <div class="plan-item">Ofrecer traslados</div>
-        <div class="plan-item">Realizar donaciones</div>
-        <div class="plan-item">Ayudar en emergencias</div>
       </div>
-      <div class="plan-footer">
-        <button class="button is-fullwidth">Ver más</button>
-      </div>
-    </div>
 
-    <div class="pricing-plan is-danger">
-      <div class="plan-header">Clínica Veterinaria</div>
-      <div class="plan-price">
-        <figure class="image is-48x48 inline-block m-auto">
-          <img src="../assets/png/033-doctor.png" />
-        </figure>
-      </div>
-      <div class="plan-items">
-        <div class="plan-item">
-          <strong>¡ES GRATIS!</strong>
+      <!-- Veterinaria -->
+      <div class="flex-1 m-2 card text-center">
+        <header class="p-4">
+          <span class="title is-5">Refugio</span>
+          <figure class="image is-48x48 mt-4 mx-auto">
+            <img src="../assets/png/033-doctor.png" />
+          </figure>
+        </header>
+        <div class="content">
+          <div class="bg-gray-100 border-b-2 border-white p-2">
+            <strong>¡ES GRATIS!</strong>
+          </div>
+          <div class="bg-gray-100 border-b-2 border-white p-2">
+            <span class="subtitle is-6">Publicar servicios</span>
+          </div>
+          <div class="bg-gray-100 border-b-2 border-white p-2">
+            Recibir donaciones
+          </div>
+          <div class="bg-gray-100 border-b-2 border-white p-2">
+            Ayudar a refugios
+          </div>
+          <footer class="p-2">
+            <button class="button is-fullwidth is-danger">Ver más</button>
+          </footer>
         </div>
-        <div class="plan-item">Publicar servicios</div>
-        <div class="plan-item">Recibir donaciones</div>
-        <div class="plan-item">Ayudar a refugios</div>
-      </div>
-      <div class="plan-footer">
-        <button class="button is-fullwidth">Ver más</button>
       </div>
     </div>
   </div>
