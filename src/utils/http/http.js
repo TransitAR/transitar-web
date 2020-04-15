1047c3d80dd31ccd1832154d2acb184b56cef6aa
GMaiolo <guille.maiolo@gmail.com>
2020-04-15 01:58:42 -0300

    Feat: using proxy

--- src/utils/http/http.js
+++ src/utils/http/http.js
@@ -1,26 +1,18 @@
 import axios from "axios";
 
-const baseURL = "https://api.refugiar.org/";
+export const getPersons = () => axios.get("/api/persons");
+export const getPerson = id => axios.get(`/api/persons/${id}`);
 
-const instance = axios.create({
-  baseURL
-});
+export const getPets = () => axios.get("/api/pets");
+export const getPet = id => axios.get(`/api/pets/${id}`);
 
-export const getPersons = () => instance.get("/api/persons");
-export const getPerson = id => instance.get(`/api/persons/${id}`);
+export const getRefuges = () => axios.get("/api/refuges");
+export const getRefuge = id => axios.get(`/api/refuges/${id}`);
 
-export const getPets = () => instance.get("/api/pets");
-export const getPet = id => instance.get(`/api/pets/${id}`);
-
-export const getRefuges = () => instance.get("/api/refuges");
-export const getRefuge = id => instance.get(`/api/refuges/${id}`);
-
-export const getVets = () => instance.get("/api/vets");
-export const getVet = id => instance.get(`/api/vets/${id}`);
+export const getVets = () => axios.get("/api/vets");
+export const getVet = id => axios.get(`/api/vets/${id}`);
 
 export const updateUser = (data, accessToken) =>
-  instance.patch(
-    "/api/users",
-    { headers: { Authorization: `Bearer ${accessToken}` } },
-    data
-  );
+  axios.patch("/api/users", data, {
+    headers: { Authorization: `Bearer ${accessToken}` }
+  });
