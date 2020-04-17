128e2ae8fac2fe2432f534dc0102ed2889b324e0
julietaflux <julietaflux@gmail.com>
2020-04-17 11:59:16 -0300

    Ref: about us page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,52 +1,69 @@
 <template>
   <section class="container is-fullhd p-16">
     <div class="container mt-8">
-      <h1 class="title is-1 mt-8">Quienes somos</h1>
-      <!-- Vision -->
-      <div class="grid grid-cols-2">
-        <div>
-          <p>
-            Pellentesque lacinia vel diam ac ultrices. Nam hendrerit tortor
-            pharetra commodo pulvinar. Cras vitae nisl vulputate, tincidunt
-            lacus pretium, mattis risus. Vestibulum ante ipsum primis in
-            faucibus orci luctus et ultrices posuere cubilia Curae; Morbi
-            posuere odio et sagittis pretium. Duis vestibulum a felis eget
-            fringilla.
-          </p>
-          <hr />
+      <div class="columns is-centered is-vcentered">
+        <div class="column is-two-thirds">
+          <div class="grid">
+            <div>
+              <h2 class="title is-1 mt-8">Quienes somos</h2>
+              <p>
+                Pellentesque lacinia vel diam ac ultrices. Nam hendrerit tortor
+                pharetra commodo pulvinar. Cras vitae nisl vulputate, tincidunt
+                lacus pretium, mattis risus.
+              </p>
+              <hr />
+            </div>
+          </div>
+          <!-- Vision -->
+          <div class="grid">
+            <div>
+              <h2 class="title">Visión</h2>
+              <p>
+                Pellentesque lacinia vel diam ac ultrices. Nam hendrerit tortor
+                pharetra commodo pulvinar. Cras vitae nisl vulputate, tincidunt
+                lacus pretium, mattis risus.
+              </p>
+              <hr />
+            </div>
+          </div>
+          <!-- Mision -->
+          <div class="grid">
+            <div>
+              <h2 class="title">Misión</h2>
+              <p>
+                Sed eleifend elit vel quam aliquet aliquam. In nec nisi dapibus,
+                efficitur ipsum vel, sagittis ante. Class aptent taciti sociosqu ad
+                litora torquent per conubia nostra, per inceptos himenaeos.
+              </p>
+              <hr />
+            </div>
+          </div>
+          <!-- Valores -->
+          <div class="grid">
+            <div>
+              <h2 class="title">Valores</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                egestas egestas libero ut mattis. Morbi luctus a dui venenatis
+                gravida. Nulla sit amet velit eget risus efficitur varius.
+              </p>
+              <hr />
+            </div>
+          </div>
         </div>
-        <div class="flex justify-center">
-          <h2 class="title">Vision</h2>
-        </div>
-      </div>
-      <!-- Mision -->
-      <div class="grid grid-cols-2">
-        <div>
-          <p>
-            Sed eleifend elit vel quam aliquet aliquam. In nec nisi dapibus,
-            efficitur ipsum vel, sagittis ante. Class aptent taciti sociosqu ad
-            litora torquent per conubia nostra, per inceptos himenaeos.
-          </p>
-          <hr />
-        </div>
-        <div class="flex justify-center">
-          <h2 class="title">Mision</h2>
-        </div>
-      </div>
-      <!-- Valores -->
-      <div class="grid grid-cols-2">
-        <div>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-            egestas egestas libero ut mattis. Morbi luctus a dui venenatis
-            gravida. Nulla sit amet velit eget risus efficitur varius.
-            Suspendisse scelerisque finibus justo, a ultrices justo malesuada
-            vitae.
-          </p>
-          <hr />
-        </div>
-        <div class="flex justify-center">
-          <h2 class="title">Valores</h2>
+        <div class="column is-narrow has-text-centered">
+          <div class="grid grid-cols-1">
+            <div>
+              <figure class="image is-128x128">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+            </div>
+            <div>
+              <figure class="image is-128x128">
+                <img src="https://bulma.io/images/placeholders/128x128.png" />
+              </figure>
+            </div>
+          </div>
         </div>
       </div>
     </div>
