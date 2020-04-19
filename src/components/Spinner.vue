90d34c324f9ff6ae4235ba0dae7a8212bef79a12
GMaiolo <guille.maiolo@gmail.com>
2020-04-19 19:22:00 -0300

    Feat: added spinner component

---
+++ src/components/Spinner.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="spinner"
+    :style="{
+      height: size,
+      width: size
+    }"
+  ></div>
+</template>
+
+<script>
+export default {
+  name: "Spinner",
+  props: {
+    size: {
+      type: String,
+      default: "1em"
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.spinner {
+  animation: spinAround 500ms infinite linear;
+  border: 2px solid #00d1b2;
+  border-radius: 100%;
+  border-right-color: transparent;
+  border-top-color: transparent;
+  display: block;
+}
+</style>
