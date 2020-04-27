2d27bd99ea9ba82c47064af10c6657e22f9569ec
GMaiolo <guille.maiolo@gmail.com>
2020-04-27 00:52:52 -0300

    Feat: added ToggleField

---
+++ src/components/inputs/ToggleField.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="field">
+    <div class="control">
+      <input
+        :id="`toggle-option-${name}`"
+        type="checkbox"
+        class="switch"
+        :name="name"
+        :checked="checked"
+        @input="onInput"
+      />
+      <label :for="`toggle-option-${name}`">
+        {{ label }}
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Toggle",
+  props: {
+    label: String,
+    name: {
+      type: String,
+      required: true
+    },
+    checked: {
+      type: Boolean,
+      required: true
+    }
+  },
+  methods: {
+    onInput($event) {
+      this.$emit("input", $event);
+    }
+  }
+};
+</script>
