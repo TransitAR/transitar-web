08ad417cef62db392a23ba583f4bb8179baaf72c
GMaiolo <guille.maiolo@gmail.com>
2020-04-27 00:52:16 -0300

    Feat: added CheckboxField

---
+++ src/components/inputs/CheckboxField.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="field">
+    <div class="control">
+      <input
+        class="is-checkradio has-background-color"
+        type="checkbox"
+        :id="`checkbox-option-${name}`"
+        :name="name"
+        :checked="checked"
+        @input="onInput"
+      />
+      <label :for="`checkbox-option-${name}`">{{ label }}</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RadioGroup",
+  props: {
+    label: String,
+    name: {
+      type: String,
+      required: true
+    },
+    checked: {
+      type: Boolean,
+      required: true
+    }
+  },
+  methods: {
+    onInput($event) {
+      this.$emit("input", $event);
+    }
+  }
+};
+</script>
