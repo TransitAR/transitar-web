65bfba8f80a170587cf9048e0d54108cf8a36117
GMaiolo <guille.maiolo@gmail.com>
2020-04-27 00:52:31 -0300

    Feat: added PhoneField

---
+++ src/components/inputs/PhoneField.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="field">
+    <div class="field-body">
+      <div class="field is-expanded">
+        <div class="field has-addons">
+          <p class="control">
+            <a class="button is-static">{{ areaCode }}</a>
+          </p>
+          <p class="control is-expanded">
+            <input
+              class="input"
+              type="tel"
+              :placeholder="placeholder"
+              :value="value"
+              @input="onInput"
+            />
+          </p>
+        </div>
+        <p class="help">{{ helpText }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PhoneField",
+  props: {
+    placeholder: String,
+    helpText: String,
+    areaCode: {
+      type: String,
+      default: "+54"
+    },
+    value: {
+      // comentando el type porque String vs Number y no tengo ganas
+      // type: String,
+      required: true
+    }
+  },
+  methods: {
+    onInput($event) {
+      this.$emit("input", $event);
+    }
+  }
+};
+</script>
