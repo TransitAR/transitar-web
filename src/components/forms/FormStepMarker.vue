36be1df1df07134253a10b4b56e198ace44ef270
GMaiolo <guille.maiolo@gmail.com>
2020-05-02 18:58:00 -0300

    Feat: more componentization

---
+++ src/components/forms/FormStepMarker.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="step-item is-primary"
+    :class="{
+      'is-active': isActive(number),
+      'is-completed': isCompleted(number)
+    }"
+  >
+    <div class="step-marker">{{ number }}</div>
+    <div class="step-details">
+      <p class="step-title">{{ marker }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FormStepMarker",
+  props: {
+    marker: String,
+    number: Number,
+    currentStep: Number
+  },
+  methods: {
+    isActive(step) {
+      return step <= this.currentStep;
+    },
+    isCompleted(step) {
+      return step < this.currentStep;
+    }
+  }
+};
+</script>
