01362e3f7bc1a9a30be3f1c8a675f6f5fce9d550
GMaiolo <guille.maiolo@gmail.com>
2020-05-02 18:17:26 -0300

    Fix: forgot to add file

---
+++ src/components/register/VerifyEmail.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="text-center">
+    <h2 class="subtitle">
+      Tenes que verificar to correo para poder continuar.
+    </h2>
+    <button class="button is-primary" @click="reload()">
+      Ya lo verifiqué
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotVerified",
+  methods: {
+    reload() {
+      window.location.reload();
+    }
+  }
+};
+</script>
